829377c17c1cc08c94b141e16660c2baa3caa8cc
aihtnyc <111184537+aihtnyc-h@users.noreply.github.com>
2023-03-27 13:10:10 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/finalproject.jar"
+PROJECT_ROOT="/home/ubuntu/app/build/libs"
+JAR_FILE="$PROJECT_ROOT/DDAL-GGAK-BE.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
